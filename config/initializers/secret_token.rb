91f603ee2e9241ba6415b9e6e40ceceed7ca7ee0
Matt Atkins <matt@yoomee.com>
2013-12-02 12:26:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasPartnerPolling::Application.config.secret_token = '41fe2b34a5bcad24541663815393973b5b94a3df7b563d597fa548c000dbe436c6827fc6bfd4af59678516afffc1f73895ca65ee7cae9262efb8b897f49578ed'
