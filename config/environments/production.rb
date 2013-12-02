78b17b0dcfd824cf397d3d6a4541551ed732d7fd
Matt Atkins <matt@yoomee.com>
2013-12-02 13:10:41 +0000

    Add HTTP auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ MasPartnerPolling::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Protect with HTTP Auth
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
+    [u, p] == ['mas', 'orangespanner']
+  end
 end
