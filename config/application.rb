6061793dcd042b97d0ec37e7f981daa4ad1d1f1f
Matt Atkins <matt@yoomee.com>
2013-12-06 11:41:35 +0000

    Update partner_polling and add assets

--- config/application.rb
+++ config/application.rb
@@ -60,8 +60,6 @@ module MasPartnerPolling
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
-    # Don't connect to database during precompile
-    config.assets.initialize_on_precompile = false
+
   end
 end
