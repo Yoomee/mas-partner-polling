85df349e5dfca83dd0f1893e8ece7312cdcbc32b
Matt Atkins <matt@yoomee.com>
2013-12-11 11:15:43 +0000

    Update partner_polling for refactored polls and widgets

--- db/schema.rb
+++ db/schema.rb
@@ -11,34 +11,56 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131203101840) do
+ActiveRecord::Schema.define(:version => 20131210102813) do
+
+  create_table "partner_polling_partners", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "partner_polling_polls", :force => true do |t|
     t.string   "name"
     t.text     "question"
     t.string   "answer_1"
-    t.text     "message_1"
-    t.text     "url_1"
     t.string   "answer_2"
-    t.text     "message_2"
-    t.text     "url_2"
     t.string   "answer_3"
-    t.text     "message_3"
-    t.text     "url_3"
     t.string   "answer_4"
-    t.text     "message_4"
-    t.text     "url_4"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "partner_polling_votes", :force => true do |t|
-    t.integer  "poll_id"
+    t.integer  "widget_id"
     t.integer  "answer_number"
     t.boolean  "redirected",    :default => false
     t.datetime "created_at"
   end
 
-  add_index "partner_polling_votes", ["poll_id"], :name => "index_partner_polling_votes_on_poll_id"
+  add_index "partner_polling_votes", ["widget_id"], :name => "index_partner_polling_votes_on_widget_id"
+
+  create_table "partner_polling_widgets", :force => true do |t|
+    t.integer  "poll_id"
+    t.integer  "partner_id"
+    t.text     "call_to_action_text_1"
+    t.text     "call_to_action_url_1"
+    t.string   "color_1"
+    t.text     "call_to_action_text_2"
+    t.text     "call_to_action_url_2"
+    t.string   "color_2"
+    t.text     "call_to_action_text_3"
+    t.text     "call_to_action_url_3"
+    t.string   "color_3"
+    t.text     "call_to_action_text_4"
+    t.text     "call_to_action_url_4"
+    t.string   "color_4"
+    t.text     "default_call_to_action_text"
+    t.text     "default_call_to_action_url"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+  end
+
+  add_index "partner_polling_widgets", ["partner_id"], :name => "index_partner_polling_widgets_on_partner_id"
+  add_index "partner_polling_widgets", ["poll_id"], :name => "index_partner_polling_widgets_on_poll_id"
 
 end
