c134d36c45b901571bada03977a3de74592f987d
Matt Atkins <matt@yoomee.com>
2014-01-10 13:24:08 +0000

    Add SendGrid details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,17 @@ MasPartnerPolling::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Protect with HTTP Auth
   config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
