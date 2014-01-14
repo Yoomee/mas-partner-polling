07535a6280576134f9f9d9ef002e4bf806f9930b
Matt Atkins <matt@yoomee.com>
2014-01-14 11:10:06 +0000

    Update partner_polling to move settings into app

---
+++ config/initializers/partner_polling.rb
@@ -0,0 +1,5 @@
+PartnerPolling.setup do |config|
+  config.google_analytics_domain = 'mas-partner-polling.herokuapp.com'
+  config.google_analytics_tracking_id = 'UA-29793933-2'
+  config.devise_mailer_sender = 'admin@mas-partner-polling.herokuapp.com'
+end
